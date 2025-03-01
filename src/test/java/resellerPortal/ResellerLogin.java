6e6990ba40893f351b0372784da51d82b7927562
goluhello <ppgolu2@gmail.com>
2025-03-01 21:51:04 +0530

    Setup ready for Jeknins and login/logout feature done these file

---
+++ src/test/java/resellerPortal/ResellerLogin.java
@@ -0,0 +1,85 @@
+package resellerPortal;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+import java.util.List;
+
+public class ResellerLogin extends baseClass {
+    private static final Logger logger = LogManager.getLogger(ResellerLogin.class);
+    private static WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeClass
+    public void setup() {
+        logger.info("Initializing WebDriver...");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(base_url);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Explicit wait
+        logger.info("Navigated to Reseller Login Page. Page title: " + driver.getTitle());
+    }
+
+    @Test(priority = 1)
+    public void loginWithMobileNumber() {
+        logger.info("Executing test: loginWithMobileNumber");
+
+        WebElement mobileInput = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[contains(@placeholder,'0000000000')]")));
+        mobileInput.sendKeys(resellerMoNo);
+        logger.info("Reseller mobile number entered.");
+
+        WebElement generateOtpButton = driver.findElement(By.xpath("//button[contains(text(),'Generate OTP')]"));
+        generateOtpButton.click();
+        logger.info("OTP generated successfully.");
+
+        List<WebElement> otpFields = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//input[contains(@aria-label, 'Digit')]")));
+
+        for (int i = 0; i < otp.length(); i++) {
+            otpFields.get(i).sendKeys(String.valueOf(otp.charAt(i)));
+        }
+        logger.info("OTP entered successfully.");
+
+        WebElement verifyOtpButton = driver.findElement(By.xpath("//button[contains(text(),'Verify OTP')]"));
+        verifyOtpButton.click();
+        logger.info("Reseller login successful.");
+    }
+
+    @Test(priority = 2)
+    public void loginWithMobileEmail() {
+        logger.info("Executing test: loginWithMobileEmail");
+        System.out.println("All done");
+    }
+
+    @Test(priority = 3)
+    public void partnerLogout() throws InterruptedException {
+        logger.info("Executing test: partnerLogout");
+
+        WebElement logoutButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(@class,'border-gray-300')]")));
+        String partnerName = logoutButton.getText();
+        logger.info("Partner name: " + partnerName);
+        Thread.sleep(5000);
+        logoutButton.click();
+        driver.findElement(By.xpath("//div[contains(text(),'Logout')]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//button[contains(text(),'Yes')]")).click();
+        logger.info("Partner logged out successfully.");
+    }
+
+    @AfterClass
+    public void teardown() throws InterruptedException {
+        if (driver != null) {
+            logger.info("Closing browser...");
+            Thread.sleep(5000);
+            driver.quit();
+            logger.info("Browser closed successfully.");
+        }
+    }
+}
--- src/test/java/resellerPortal/baseClass.java
+++ src/test/java/resellerPortal/baseClass.java
@@ -2,5 +2,7 @@ package resellerPortal;
 
 public class baseClass {
     String base_url = "https://uat-ui-reseller.web.app/";
+    String resellerMoNo="9650388341";
+    String otp = "2343";
 
 }
